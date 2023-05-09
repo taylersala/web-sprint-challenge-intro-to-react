9962aaa53f9202b2b7f7d02ad6d9b090273b9d97
taylersala <taylersala@gmail.com>
2023-05-09 13:23:30 -0700

    started brainstorming

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+
+
+
+function Character({ name, birth_year }) {
+const [name, setName] = useState;
+const fetchName = (id) => {
+    fetch(`https://swapi.dev/api/people/${id}`)
+    .then(res => res.json())
+    console.log(res.data); 
+    return (
+        <div className="user">
+            <div>{name}</div>
+        </div>
+    )
+}
+}
+
+
+// https://swapi.dev/api/people/
